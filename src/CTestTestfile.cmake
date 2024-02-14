16115697093ba41b5cec2bf02fd7167882f88d57
Simon Thrane Hansen <sthranehansen@gmail.com>
2024-02-14 08:44:03 +0100

    Removed spin generated files

--- src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/slazreg/Work/Research/Daedalux/src
-# Build directory: /home/slazreg/Work/Research/Daedalux/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
