7f65f61585bba1a5b3bd89c24c1b4ec1979e392c
Simon Thrane Hansen <sthranehansen@gmail.com>
2024-02-22 12:13:53 +0000

    Added test of post Method

--- tests/test_state.cpp
+++ tests/test_state.cpp
@@ -1,61 +1,94 @@
-#include <gtest/gtest.h>
 #include "../src/core/semantic/variable/state/composite.hpp"
+#include <gtest/gtest.h>
 
 class StateTest : public ::testing::Test {
 protected:
-    void SetUp() override {
-        // Common setup code for all tests goes here
-        s = std::make_unique<composite>("testState");
-    }
+  void SetUp() override
+  {
+    // Common setup code for all tests goes here
+    s = std::make_unique<composite>("testState");
+  }
 
-    void TearDown() override {
-        // Common teardown code for all tests goes here
-    }
-    std::unique_ptr<state> s;
+  void TearDown() override
+  {
+    // Common teardown code for all tests goes here
+  }
+  std::unique_ptr<state> s;
+  std::string array_model = "/test_files/basic/array.pml";
+  std::string flow_model = "/test_files/basic/flows.pml";
+  std::string current_path = std::filesystem::current_path();
 };
 
-TEST_F(StateTest, DefaultConstructor) {
-    EXPECT_EQ(variable::Type::V_COMP_S, s->getType());
-    EXPECT_EQ("testState", s->getFullName());
-    EXPECT_EQ(1.0, s->getProbability());
-    EXPECT_EQ(nullptr, s->getOrigin());
-    EXPECT_EQ(0u, s->getErrorMask());
-    EXPECT_TRUE(s->executables().empty());
-    EXPECT_TRUE(s->hasDeadlock());
-    EXPECT_EQ(0u, s->getErrorMask());
+TEST_F(StateTest, DefaultConstructor)
+{
+  EXPECT_EQ(variable::Type::V_COMP_S, s->getType());
+  EXPECT_EQ("testState", s->getFullName());
+  EXPECT_EQ(1.0, s->getProbability());
+  EXPECT_EQ(nullptr, s->getOrigin());
+  EXPECT_EQ(0u, s->getErrorMask());
+  EXPECT_TRUE(s->executables().empty());
+  EXPECT_TRUE(s->hasDeadlock());
+  EXPECT_EQ(0u, s->getErrorMask());
 }
 
-TEST_F(StateTest, CopyConstructor) {
-    // Create a state object to copy from
-    auto originalState = new composite("test_variable");
-    originalState->prob = 0.5;
+TEST_F(StateTest, CopyConstructor)
+{
+  // Create a state object to copy from
+  auto originalState = new composite("test_variable");
+  originalState->prob = 0.5;
 
-    // Create a new state object using the copy constructor
-    // auto copiedState = new composite(originalState);
+  // Create a new state object using the copy constructor
+  // auto copiedState = new composite(originalState);
 
-    // Verify that the copied state has the same values as the original state
-    // EXPECT_EQ(copiedState->prob, originalState->prob);
-    // EXPECT_EQ(copiedState->origin, originalState->origin);
-    // EXPECT_EQ(copiedState->errorMask, originalState->errorMask);
+  // Verify that the copied state has the same values as the original state
+  // EXPECT_EQ(copiedState->prob, originalState->prob);
+  // EXPECT_EQ(copiedState->origin, originalState->origin);
+  // EXPECT_EQ(copiedState->errorMask, originalState->errorMask);
 
-    // delete copiedState;
-    // delete originalState;
+  // delete copiedState;
+  // delete originalState;
 }
 
-// TEST_F(StateTest, CopyConstructorWithErrors) {
-//     auto original = new composite("originalState");
-//     original->addError(state::ERR_DEADLOCK);
+TEST_F(StateTest, PostStateArray)
+{
+  const TVL * tvl = nullptr;
+  auto file_path1 = current_path + array_model;
+  auto loader = std::make_unique<promela_loader>(file_path1, tvl);
+  auto myFSM = loader->getAutomata().get();
+  auto current_state = initState::createInitState(myFSM, tvl);
+  state * next_state = nullptr;
+  bool new_state_found = false;
+  int search_depth = 10;
+  for (auto i = 0; i < search_depth; i++) {
+    next_state = current_state->Post().front();
+    if (!next_state->isSame(current_state)) {
+      new_state_found = true;
+      break;
+    }
+    current_state = next_state;
+  }
+  ASSERT_TRUE(new_state_found);
+}
 
-//     auto copy = new composite(original);
 
-//     EXPECT_EQ(original->getType(), copy->getType());
-//     EXPECT_EQ(original->getFullName(), copy->getFullName());
-//     EXPECT_EQ(original->getProbability(), copy->getProbability());
-//     EXPECT_EQ(nullptr, copy->getOrigin()); // Origin should be reset in the copy
-//     EXPECT_EQ(original->getErrorMask(), copy->getErrorMask());
-//     EXPECT_FALSE(copy->hasDeadlock()); // A copy should not inherit executables
-//     EXPECT_EQ(state::ERR_DEADLOCK, copy->getErrorMask());
-
-//     delete copy;
-//     delete original;
-// }
+TEST_F(StateTest, PostStateFlow)
+{
+  const TVL * tvl = nullptr;
+  auto file_path1 = current_path + flow_model;
+  auto loader = std::make_unique<promela_loader>(file_path1, tvl);
+  auto myFSM = loader->getAutomata().get();
+  auto current_state = initState::createInitState(myFSM, tvl);
+  state * next_state = nullptr;
+  bool new_state_found = false;
+  int search_depth = 10;
+  for (auto i = 0; i < search_depth; i++) {
+    auto successors = current_state->Post();
+    next_state = successors.front();
+    if (!next_state->isSame(current_state)) {
+      new_state_found = true;
+      break;
+    }
+    current_state = next_state;
+  }
+  ASSERT_TRUE(new_state_found);
+}
